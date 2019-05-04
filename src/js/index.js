c15c6955b3a7c9933ac070978767debeda002c48
Maksim <maaa23@mail.ru>
2019-05-04 19:23:07 +0400

    add data-dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,65 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+import $ from 'jquery';
+import '../../node_modules/pikaday/css/pikaday.css';
 import '../scss/main.scss';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import datepicker from 'jquery-datepicker';
+
+
+// datepicker($);
+
+
+// datepicker($);
+// $(function() {
+//   const dates = $('#from, #to').datepicker({
+//     onSelect: function ( selectedDate ) {
+//       const option = this.id === "from" ? "minDate" : "maxDate",
+// 	    instance = $( this ).data( "datepicker" ),
+// 	    date = $.datepicker.parseDate(
+// 	instance.settings.dateFormat ||
+// 	$.datepicker._defaults.dateFormat,
+// 	selectedDate, instance.settings );
+// 	dates.not( this ).datepicker( "option", option, date );
+// 	}
+//   });
+// });
