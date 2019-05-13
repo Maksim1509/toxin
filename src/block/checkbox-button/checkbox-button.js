eb97254f514b1640e5c6a1469e37665246acb5a9
Maksim <maaa23@mail.ru>
2019-05-13 15:24:34 +0400

    add blok: checkbox-button

---
+++ src/block/checkbox-button/checkbox-button.js
@@ -0,0 +1,16 @@
+const checkboxController = () => {
+  
+  const checkboxContent = document.querySelectorAll('.checkbox-button__content');
+  const checkbox = document.querySelectorAll('.checkbox-button__button');
+
+  for (let i = 0; i < checkboxContent.length; i += 1) {
+    checkboxContent[i].addEventListener('click', () => {
+      if (checkbox[i].classList.contains('checkbox-button__button--checked')) {
+        checkbox[i].classList.remove('checkbox-button__button--checked');
+      } else {
+        checkbox[i].classList.add('checkbox-button__button--checked');
+      }
+    });
+  }
+};
+checkboxController();
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@
 
 import '../scss/main.scss';
 import '../block/dropdown/button-dropdown';
+import '../block/checkbox-button/checkbox-button';
