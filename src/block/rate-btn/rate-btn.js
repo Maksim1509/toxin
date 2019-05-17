3cba6b2786777c12d77ffd3c0563e0172a0baf2f
Maksim <maaa23@mail.ru>
2019-05-17 10:07:43 +0400

    add js to rate btn

---
+++ src/block/rate-btn/rate-btn.js
@@ -0,0 +1,70 @@
+const star = document.querySelectorAll('.rate-btn__star');
+
+const star1 = document.getElementById('star1');
+const star2 = document.getElementById('star2');
+const star3 = document.getElementById('star3');
+const star4 = document.getElementById('star4');
+const star5 = document.getElementById('star5');
+const starFull = 'rate-btn__star--full';
+
+star1.addEventListener('click', () => {
+  if (star1.classList.contains(starFull)) {
+    for (let i = 1; i < 5; i += 1) {
+      if (star[i].classList.contains(starFull)) {
+        star[i].classList.remove(starFull);
+      }
+    }
+  } else {
+    star1.classList.add(starFull);
+  }
+});
+
+star2.addEventListener('click', () => {
+  if (star2.classList.contains(starFull)) {
+    for (let i = 2; i < 5; i += 1) {
+      if (star[i].classList.contains(starFull)) {
+        star[i].classList.remove(starFull);
+      }
+    }
+  } else {
+    star2.classList.add(starFull);
+    star1.classList.add(starFull);
+  }
+});
+
+star3.addEventListener('click', () => {
+  if (star3.classList.contains(starFull)) {
+    for (let i = 3; i < 5; i += 1) {
+      if (star[i].classList.contains(starFull)) {
+        star[i].classList.remove(starFull);
+      }
+    }
+  } else {
+    star3.classList.add(starFull);
+    star2.classList.add(starFull);
+    star1.classList.add(starFull);
+  }
+});
+
+star4.addEventListener('click', () => {
+  if (star4.classList.contains(starFull)) {
+    if (star[4].classList.contains(starFull)) {
+      star[4].classList.remove(starFull);
+    }
+  } else {
+    star4.classList.add(starFull);
+    star3.classList.add(starFull);
+    star2.classList.add(starFull);
+    star1.classList.add(starFull);
+  }
+});
+
+star5.addEventListener('click', () => {
+  if (!star5.classList.contains(starFull)) {
+    star5.classList.add(starFull);
+    star4.classList.add(starFull);
+    star3.classList.add(starFull);
+    star2.classList.add(starFull);
+    star1.classList.add(starFull);
+  }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,4 @@ import '../scss/main.scss';
 import '../block/dropdown/button-dropdown';
 import '../block/checkbox-button/checkbox-button';
 import '../block/like-btn/like-btn';
+import '../block/rate-btn/rate-btn';
