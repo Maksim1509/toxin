53ef12d2bad03d4d3bbca7d9c3082d6c96b2e3e7
Maksim <maaa23@mail.ru>
2019-06-15 10:53:37 +0400

    create block feedback

--- src/block/like-btn/like-btn.js
+++ src/block/like-btn/like-btn.js
@@ -1,11 +1,17 @@
-const likeBtn = document.querySelector('.like-btn__input');
-const count = document.querySelector('.like-btn__count');
+const likeBtn = document.querySelectorAll('.like-btn__wrap');
 
+const elems = Array.from(likeBtn);
 
-likeBtn.addEventListener('change', () => {
-  if (likeBtn.checked) {
-    count.textContent = Number(count.textContent) + 1;
-  } else {
-    count.textContent = Number(count.textContent) - 1;
-  }
-});
+const contolLikeBtn = () => {
+  elems.map(elem => elem.addEventListener('click', () => {
+    const count = elem.querySelector('.like-btn__count');
+    const inputCheckbox = elem.previousSibling;
+    if (inputCheckbox.checked) {
+      count.textContent = Number(count.textContent) - 1;
+    } else {
+      count.textContent = Number(count.textContent) + 1;
+    }
+  }));
+};
+
+contolLikeBtn();
