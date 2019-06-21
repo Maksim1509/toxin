af511789b69e112875d17a9f5fed6cc9c117dfcc
Maksim <maaa23@mail.ru>
2019-06-21 19:26:00 +0400

    add style to datepicker

--- src/block/form-date/form-date.js
+++ src/block/form-date/form-date.js
@@ -2,4 +2,12 @@ import $ from 'jquery';
 import airDatapicker from 'air-datepicker';
 
 const formDates = $('.form-date');
-Array.from(formDates).map(formDate => $(formDate).datepicker());
+Array.from(formDates).map(formDate => $(formDate).datepicker({
+    inline: true,
+    navTitles: {
+        days: 'MM <i>yyyy</i>'
+    },
+    prevHtml: '<i class="material-icons">arrow_back</i>',
+    nextHtml: '<i class="material-icons">arrow_forward</i>',
+    clearButton: true
+}));
