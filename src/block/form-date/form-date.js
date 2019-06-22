45d0b3a47437059f425c18fa5a2f0bd05de74181
Maksim <maaa23@mail.ru>
2019-06-22 12:12:17 +0400

    добавил еще стилей к календарю

--- src/block/form-date/form-date.js
+++ src/block/form-date/form-date.js
@@ -3,11 +3,25 @@ import airDatapicker from 'air-datepicker';
 
 const formDates = $('.form-date');
 Array.from(formDates).map(formDate => $(formDate).datepicker({
-    inline: true,
-    navTitles: {
-        days: 'MM <i>yyyy</i>'
-    },
-    prevHtml: '<i class="material-icons">arrow_back</i>',
-    nextHtml: '<i class="material-icons">arrow_forward</i>',
-    clearButton: true
+  navTitles: {
+    days: 'MM <i>yyyy</i>',
+  },
+  prevHtml: '<i class="material-icons">arrow_back</i>',
+  nextHtml: '<i class="material-icons">arrow_forward</i>',
+  clearButton: true,
 }));
+
+
+const addApply = () => {
+  const datapickers = document.querySelectorAll('.datepicker');
+  Array.from(datapickers).map((datapicker) => {
+    const buttons = datapicker.querySelector('.datepicker--buttons');
+    const applyBtn = document.createElement('span');
+    applyBtn.classList.add('datepicker--button');
+    applyBtn.classList.add('-apply-');
+    applyBtn.textContent = 'применить';
+    buttons.appendChild(applyBtn);
+    return true;
+  });
+};
+addApply();
