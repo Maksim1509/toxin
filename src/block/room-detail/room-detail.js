4e221c6b84283dfd1b9f0740dddd1ba091d6e753
Maksim <maaa23@mail.ru>
2019-07-16 20:44:06 +0400

    создал страницу room detail

---
+++ src/block/room-detail/room-detail.js
@@ -0,0 +1,5 @@
+import '../../scss/main.scss';
+import '../dropdown/button-dropdown';
+import '../like-btn/like-btn';
+import '../form-filter-dropdown/form-filter-dropdown';
+import '../form-date-dropdown/form-date-dropdown';
