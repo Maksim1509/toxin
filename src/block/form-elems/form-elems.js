6f0caeb6135257312c2544f19474eee36a17dd4d
Maksim <maaa23@mail.ru>
2019-07-21 11:34:30 +0400

    начал верстать страницу form-elems, сверстал левую колонку

---
+++ src/block/form-elems/form-elems.js
@@ -0,0 +1,8 @@
+import '../../scss/main.scss';
+import '../dropdown/button-dropdown';
+import '../form-date/form-date';
+import '../form-date-dropdown/form-date-dropdown';
+import '../form-filter-dropdown/form-filter-dropdown';
+import '../rooms-filter/rooms-filter';
+import '../checkbox-list/checkbox-list';
+import '../checkbox-button/input-chbox/input-chbox';
