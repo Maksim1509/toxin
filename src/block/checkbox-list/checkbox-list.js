e0411105a31d180200b032b1c2e2178888af8a80
Maksim <maaa23@mail.ru>
2019-07-23 11:56:42 +0400

    добавил несколько елементов на страницу form elems

--- src/block/checkbox-list/checkbox-list.js
+++ src/block/checkbox-list/checkbox-list.js
@@ -1,17 +1,17 @@
 const expandCheckbox = () => {
   const icon = document.querySelector('.checkbox-list__icon');
-  const title = document.querySelector('.checkbox-list__title');
-  const checkboxes = document.querySelectorAll('.checkbox-list__content');
-  title.addEventListener('click', () => {
-    if (title.classList.contains('expand')) {
+  const title = document.querySelectorAll('.checkbox-list__title');
+  Array.from(title).map(el => el.addEventListener('click', (e) => {
+    const checkboxes = e.target.parentNode.parentNode.querySelectorAll('.checkbox-list__content');
+    if (el.classList.contains('expand')) {
       Array.from(checkboxes).map(elem => elem.classList.remove('checkbox-list__content--show'));
       icon.textContent = 'expand_more';
-      title.classList.remove('expand');
+      el.classList.remove('expand');
     } else {
       Array.from(checkboxes).map(elem => elem.classList.add('checkbox-list__content--show'));
       icon.textContent = 'expand_less';
-      title.classList.add('expand');
+      el.classList.add('expand');
     }
-  });
+  }));
 };
 expandCheckbox();
