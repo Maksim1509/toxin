f5b713e6870aac398bae0edd5caf9df2e5b47681
Maksim <maaa23@mail.ru>
2019-07-29 16:27:06 +0400

    fix dropdown

--- src/block/dropdown/button-dropdown.js
+++ src/block/dropdown/button-dropdown.js
@@ -2,7 +2,8 @@ const showDropdown = () => {
   const dp = document.querySelectorAll('.dropdown__header');
 
   Array.from(dp).map(el => el.addEventListener('click', () => {
-    const drop = el.nextSibling.querySelector('.dropdown__body');
+    const drop = el.nextSibling;
+    console.log(drop);
     if (drop.classList.contains('dropdown__body--drop')) {
       drop.classList.remove('dropdown__body--drop');
     } else {
@@ -39,7 +40,7 @@ const  aplly = () => {
     const values = e.target.parentNode.parentNode.querySelectorAll('.dropdown__value');
     const result = Array.from(values).reduce((acc, val) => Number(val.textContent) + acc, 0);
 
-    const totalGuests = e.target.parentNode.parentNode.parentNode.previousSibling.querySelector('span');
+    const totalGuests = e.target.parentNode.parentNode.previousSibling.querySelector('span');
     if (result === 1) {
       totalGuests.textContent = `${result} гость`;
     } else {
@@ -53,7 +54,7 @@ const clear = () => {
   const btnClear = document.querySelectorAll('.dropdown__button--clear');
 
   Array.from(btnClear).map(el => el.addEventListener('click', (e) => {
-    const totalGuests = e.target.parentNode.parentNode.parentNode.previousSibling.querySelector('span');
+    const totalGuests = e.target.parentNode.parentNode.previousSibling.querySelector('span');
     totalGuests.textContent = 'Сколько гостей';
 
     const values = e.target.parentNode.parentNode.querySelectorAll('.dropdown__value');
